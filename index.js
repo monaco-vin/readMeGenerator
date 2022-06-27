a48cb7a102d8d033893e5f9f28c4ad1d385af154
Rocket Appliances <monaco.vin@gmail.com>
2022-06-27 16:20:16 -0500

    FINISH questions and markdown generator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 var inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [
   {
@@ -13,10 +14,64 @@ const questions = [
     name: "description",
     message: "What is the description of your project?",
   },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use the application?",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "What are the protocals for contibuting work?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How do you test your application?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use?",
+    choices: [
+      {
+        name: "Apache License 2.0",
+        value: {
+          name: "Apache License 2.0",
+          url: "https://opensource.org/licenses/Apache-2.0",
+        },
+      },
+      {
+        name: "GNU General Public License v3.0",
+        value: {
+          name: "GNU General Public License v3.0",
+          url: "https://www.gnu.org/licenses/gpl-3.0",
+        },
+      },
+      {
+        name: "MIT License",
+        value: {
+          name: "MIT License",
+          url: "https://opensource.org/licenses/MIT",
+        },
+      },
+      {
+        name: "No License",
+        value: "",
+      },
+    ],
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(data) {}
+function writeToFile(data, title) {
+  fs.writeFile(title + ".md", data);
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -25,7 +80,7 @@ function init() {
     .then((answers) => {
       console.log(answers);
       const markdown = generateMarkdown(answers);
-      writeToFile(markdown);
+      writeToFile(markdown, answers.title);
     })
     .catch((error) => {
       console.warn(error);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,26 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == "") return "";
+  return `[![${license.name}](https://img.shields.io/apm/l/atomic-design-ui.svg?)]`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == "") return "";
+  return `(${license.url})`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return `
-  
-  `;
+  return renderLicenseBadge(license) + renderLicenseLink(license);
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-${renderLicenseSection()}
+${renderLicenseSection(data.license)}
 
   ### Description
   ${data.description}
+  ### Installation
+  ${data.installation}
+  ### Usage
+  ${data.usage}
+  ### Contribution
+  ${data.contribution}
+  ### Test
+  ${data.test}
+
 `;
 }
 
